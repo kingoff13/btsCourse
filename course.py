587f415aba4b9e7d594b1318d181e39b23229c4e
kingoff13 <kingoff.d@gmail.com>
2018-05-28 12:08:16 +0000

    select assets make tuple pairs of assets get data for each pair

--- course.py
+++ course.py
@@ -37,8 +37,7 @@ def formQuery(arTrades):
     result_query = result_query + ("INSERT INTO courses (datetime, base, base_amount, quote, quote_amount, price) VALUES ")
     arTrades.reverse()
     for trade in arTrades:
-        hole = str(trade).find('inf')
-        if hole!=-1: continue #print(str(trade))
+        if 'inf' in str(trade): continue #print(str(trade))
         trade['base_amount'] = str(trade['base']).split()[0].replace(',','')
         trade['base_lit'] = str(trade['base']).split()[1]
         trade['quote_amount'] = str(trade['quote']).split()[0].replace(',','')
@@ -50,7 +49,7 @@ def formQuery(arTrades):
     return result_query
 
 #format time: 'YYYY-MM-DDTHH:MM:SS'
-def gettrades(start=False, stop=False):
+def gettrades(market, start=False, stop=False):
     step=3000
     result=[]
     trades=[]
@@ -115,26 +114,40 @@ for name, ddl in TABLES.items():
     else:
         print("OK")
 ##############################################
-def noname(base, amount):
+def noname(pair):
     try:
-        market = Market("{}:{}".format(base, amount))
+        market = Market("{}:{}".format(pair[0], pair[1]))
     except BTSExceptions.AssetDoesNotExistsException:
         #raise Exception("")
+        print("{}:{}".format(pair[0], pair[1]) + "notexist")
         return
-    query = "SELECT datetime FROM courses WHERE base='{}' AND quote='{}' ORDER BY id DESC LIMIT 1".format(base, amount)
+    query = "SELECT datetime FROM courses WHERE base='{}' AND quote='{}' ORDER BY id DESC LIMIT 1".format(pair[0], pair[1])
     cursor.execute(query)
     values = cursor.fetchall()
     if len(values)>0: time_last_deal = values[0][0].isoformat()
     else: time_last_deal = time.strftime('%Y-%m-%dT%X',time.localtime(time.time() - 259200))
-    result = gettrades(time_last_deal)
+    result = gettrades(market, time_last_deal)
     if len(result)>0:
         query = formQuery(result)
         cursor.execute(query)
         cnx.commit()
 
-noname('USD','BTS')
-noname('RUB','BTS')
-noname('UsSD','BTS')
+query = "SELECT id, asset_id, symbol FROM p2pbridge_assets WHERE ACTIVE='Y'"
+cursor.execute(query)
+assets = cursor.fetchall()
+pairs=[]
+for i in range(len(assets)-1):
+    item = assets.pop(0)
+    for value in assets:
+        base=item[1] if (item[1].strip() and item[1]!='0') else item[2]
+        quote=value[1] if (value[1].strip() and value[1]!='0') else value[2]
+        pairs.append((base,quote))
+for pair in pairs:
+    noname(pair)
+#print('\n'.join(pairs))
+#noname('USD','BTS')
+#noname('RUB','BTS')
+#noname('UsSD','BTS')
 
 #query = "SELECT datetime, base, base_amount, quote, quote_amount, price FROM courses"
 #cursor.execute(query)

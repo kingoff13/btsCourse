fdb303e678afa0891644b5ba532948875af0689a
kingoff13 <kingoff.d@gmail.com>
2018-06-01 11:17:57 +0000

    assets like integer ids

--- course.py
+++ course.py
@@ -6,132 +6,143 @@ import os
 import yaml
 from init import DBDriver
 
-#sudo docker run -p 3306:3306 --name mysql-server -e MYSQL_ROOT_PASSWORD=notalchemy -d mysql:latest
-#sudo docker container start mysql-server
+# sudo docker run -p 3306:3306 --name mysql-server -e MYSQL_ROOT_PASSWORD=notalchemy -d mysql:latest
+# sudo docker container start mysql-server
+# sudo docker exec -it mysql-server mysql -uroot -pnotalchemy --database=bitshares
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with open('app.conf') as f:
   conf = yaml.safe_load(f.read())
-sql_conf=conf.get('sql')
-assets = conf.get('assets')
-dbConnector = DBDriver(sql_conf)
+sql_conf = conf.get('sql')
+check_interval = conf.get('check_interval', 300)
 
 def formQuery(arQuery):
-    result_query=''
+    result_query = ''
     result_query = result_query + ("INSERT INTO p2pbridge_exchange_rates (SOURCE, DATETIME, ASSET1, ASSET2, VALUE) VALUES ")
     for trade in arQuery:
-        #if 'inf' in str(trade): continue #print(str(trade))
-        #trade['base_amount'] = str(trade['base']).split()[0].replace(',','')
-        #trade['base_lit'] = str(trade['base']).split()[1]
-        #trade['quote_amount'] = str(trade['quote']).split()[0].replace(',','')
-        #trade['quote_lit'] = str(trade['quote']).split()[1]
-        #values = "('{time}','{base_lit}', {base_amount}, '{quote_lit}', {quote_amount}, {price}), ".format(**trade)
-        try: trade['base'] = asset_ids[trade['base']]
-        except: trade['base'] = 0
-        try: trade['quote'] = asset_ids[trade['quote']]
-        except: trade['quote'] = 0
-        values = "('{}', '{}', '{}', '{}', {}), ".format(trade['source'], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), trade['base'], trade['quote'], trade['value'])
-        result_query+=values
+        # if 'inf' in str(trade): continue #print(str(trade))
+        # trade['base_amount'] = str(trade['base']).split()[0].replace(',','')
+        # trade['base_lit'] = str(trade['base']).split()[1]
+        # trade['quote_amount'] = str(trade['quote']).split()[0].replace(',','')
+        # trade['quote_lit'] = str(trade['quote']).split()[1]
+        # values = "('{time}', '{base_lit}', {base_amount}, '{quote_lit}', {quote_amount}, {price}), ".format(**trade)
+        values = "('{}', '{}', '{}', '{}', {}), ".format(
+            trade['source'],
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            trade['base'],
+            trade['quote'],
+            trade['value'])
+        result_query += values
     result_query = result_query.rstrip(', ')
-    print(result_query)
     return result_query
 
-#format time: 'YYYY-MM-DDTHH:MM:SS'
-def gettrades(market, start=False, stop=False):
-    step=3000
-    result={}
-    result['quote_amount']=0
-    result['base_amount']=0
-    if stop==False: stop=time.time()
-    else: stop=time.mktime(time.strptime(stop,'%Y-%m-%dT%X'))-time.timezone
-    if start==False: start=stop-259200
-    else: start=time.mktime(time.strptime(start,'%Y-%m-%dT%X'))-time.timezone
-    while stop-step>start:
-        trades = market.trades(limit=100, start=stop-step, stop=stop)
-        if len(trades)<100:
-            stop=stop-step
-            print(time.strftime('%Y.%m.%d %X', time.localtime(stop)),
-                  len(trades),
-                  step,
-                  '*2')
-            step=step*2
-            for trade in trades:
-                result['quote_amount']+=float(str(trade['quote']).split()[0].replace(',',''))
-                result['base_amount']+=float(str(trade['base']).split()[0].replace(',',''))
-            if result['quote_amount']>1000: break
-        elif len(trades)==100:
-            print(time.strftime('%Y.%m.%d %X', time.localtime(stop)),
-                  len(trades),
-                  step,
-                  '/2')
-            step=step/2
-            if step<1:
-                stop=stop-1
-    if result['base_amount']>result['quote_amount']:
-        course = result['base_amount']/result['quote_amount']
-        base_lit = market['base']['symbol']
-        quote_lit = market['quote']['symbol']
-    else:
-        course = result['quote_amount']/result['base_amount']
-        base_lit = market['quote']['symbol']
-        quote_lit = market['base']['symbol']
-    return {'source':'Bitshares',
-            'base':base_lit,
-            'quote':quote_lit,
-            'value':course}
 
-def getDataFromBitshares(pair):
+# format time: 'YYYY-MM-DDTHH:MM:SS'
+def gettrades(market, start=False, stop=False):
+    step = 3000
+    result = {}
+    result['quote_amount'] = 0
+    result['base_amount'] = 0
+    if stop is False:
+        stop = time.time()
+    else:
+        stop = time.mktime(time.strptime(stop, '%Y-%m-%dT%X'))-time.timezone
+    if start is False:
+        start = stop-259200
+    else:
+        start = time.mktime(time.strptime(start, '%Y-%m-%dT%X'))-time.timezone
+    while stop-step > start:
+        trades = market.trades(limit=100, start=stop-step, stop=stop)
+        if len(trades) < 100:
+            stop = stop-step
+            # print(time.strftime('%Y.%m.%d %X', time.localtime(stop)),
+            #      len(trades),
+            #      step,
+            #      '*2')
+            step = step*2
+            for trade in trades:
+                result['quote_amount'] += float(str(
+                    trade['quote']).split()[0].replace(',', ''))
+                result['base_amount'] += float(str(
+                    trade['base']).split()[0].replace(',', ''))
+            if result['base_amount'] > 1000:
+                break
+        elif len(trades) == 100:
+            # print(time.strftime('%Y.%m.%d %X', time.localtime(stop)),
+            #      len(trades),
+            #      step,
+            #      '/2')
+            step = step/2
+            if step < 1:
+                stop = stop-1
     try:
-        market = Market("{}:{}".format(pair[0], pair[1]))
+        course = result['base_amount']/result['quote_amount']
+    except:
+        course = 0
+    base_lit = market['base']['symbol']
+    quote_lit = market['quote']['symbol']
+    return {'source': 'Bitshares',
+            'base': base_lit,
+            'quote': quote_lit,
+            'value': course}
+
+
+def getDataFromBitshares(base, quote='USD'):
+    try:
+        market = Market("{}:{}".format(base, quote))
     except BTSExceptions.AssetDoesNotExistsException:
-        #raise Exception("")
-        print("{}:{}".format(pair[0], pair[1]) + "notexist")
+        # raise Exception("")
+        print("{}:{}".format(base, quote) + "notexist")
         return
     return gettrades(market)
 
-def getDataFromCoinmarketcap(ids, convert=False):
-    result=[]
-    work={}
-    r = get('https://api.coinmarketcap.com/v2/listings/').json()
-    if isinstance(ids, list):
-        for key,asset in enumerate(ids):
-            for value in r['data']:
-                if value['symbol']==asset:
-                    work[asset]=value['id']
-        for key,asset in work.items():
-            r = get('https://api.coinmarketcap.com/v2/ticker/'+str(asset),
-                    {'convert':convert}).json()
-            result.append({'source':'Coinmarketcap',
-                           'base':'USD',
-                           'quote':key,
-                           'value':r['data']['quotes']['USD']['price']})
+
+def getDataFromCoinmarketcap(base, quote='USD'):
+    r = get('https://api.coinmarketcap.com/v2/ticker/'+str(base),
+            {'convert': quote}).json()
+    result = ({'source': 'Coinmarketcap',
+               'base': quote,
+               'quote': r['data']['symbol'],
+               'value': r['data']['quotes'][quote]['price']})
     return result
 
+
 def getAssetsFromMySQL():
-    result={}
-    query = "SELECT id, asset_id, symbol FROM p2pbridge_assets WHERE ACTIVE='Y'"
+    dbConnector = DBDriver(sql_conf)
+    result = []
+    query = "SELECT ID, ASSET_ID, SYMBOL, COINMARKETCAP_ID, BLOCKCHAIN_ID FROM p2pbridge_assets WHERE ACTIVE='Y' AND NOT ASSET_ID=0"
     dbConnector.cursor.execute(query)
     assets = dbConnector.cursor.fetchall()
     for asset in assets:
-        result[asset[2]]=asset[0]
+        result.append({'id': asset[0],
+                       'asset_id': asset[1],
+                       'symbol': asset[2],
+                       'coinmarketcap_id': asset[3],
+                       'blockchain_id': asset[4]})
+    dbConnector.cursor.close()
+    dbConnector.cnx.close()
     return result
 
+
+def process_loop(check_interval=300):
+    while True:
+        dbConnector = DBDriver(sql_conf)
+        dataCourses = []
+        for asset in asset_ids:
+            if isinstance(asset['coinmarketcap_id'], int):
+                a = getDataFromCoinmarketcap(asset['coinmarketcap_id'])
+                a.update({'base': 292, 'quote': asset['id']})
+                dataCourses.append(a)
+            if asset['asset_id'] != 0 and asset['blockchain_id'] == 1:
+                a = getDataFromBitshares(asset['asset_id'])
+                a.update({'base': 292, 'quote': asset['id']})
+                dataCourses.append(a)
+        query = formQuery(dataCourses)
+        dbConnector.cursor.execute(query)
+        dbConnector.cnx.commit()
+        dbConnector.cursor.close()
+        dbConnector.cnx.close()
+        time.sleep(check_interval)
+
 asset_ids = getAssetsFromMySQL()
-dataCourses = getDataFromCoinmarketcap(assets['coinmarketcap'])
-for asset in assets['bitshares']:
-    dataCourses.append(getDataFromBitshares(('USD', asset)))
-print(dataCourses)
-query = formQuery(dataCourses)
-dbConnector.cursor.execute(query)
-dbConnector.cnx.commit()
-
-#query = "SELECT datetime, base, base_amount, quote, quote_amount, price FROM courses"
-#cursor.execute(query)
-
-#for (datetime, base, base_amount, quote, quote_amount, price) in cursor:
-#    print("{} {} {} {} {} {}".format(
-#        datetime, base, base_amount, quote, quote_amount, price))
-
-#print(cursor.fetchall()[0][0].timestamp())
-dbConnector.cursor.close()
-dbConnector.cnx.close()
+process_loop(check_interval)
